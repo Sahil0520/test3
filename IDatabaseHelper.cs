9596c7b5b9be149297a7a0d4efb3a15af91aff29
Sahil0520 <126628167+Sahil0520@users.noreply.github.com>
2023-08-13 22:50:54 -0600

    Add files via upload

---
+++ IDatabaseHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoundSculptMaui
+{
+    // Interface for defining database helper functionality related to genres
+    public interface IDatabaseHelper
+    {
+        // Method to retrieve a list of all available genres from the database
+        List<string> GetGenres();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace SoundSculptMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        // Constructor for MainPage class, called when the page is created
+        public MainPage()
+        {
+            InitializeComponent(); // Initialize the page's components
+        }
+
+        // Handle start button click event
+        private async void OnStartButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GenrePage()); // Navigate to the GenrePage when start button is clicked
+        }
+    }
+}
